d45432beca1781a0c7b2cddd2df239638ed4c18b
sjw <sjwoong1010@hanmail.net>
2020-06-19 14:46:02 +0900

    수정1-1

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.header').height($(window).height())
 });
 
+
 // window.onresize = window.onload = function() {
 //     document.getElementById('header').style.height = this.innerHeight + 'px';
 // }
